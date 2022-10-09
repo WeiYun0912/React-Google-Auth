07aae8763ec427ad47d660bfefa253c1be6dcfdd
ChunWeiYeah <yher25@gmail.com>
2022-10-09 16:04:15 +0800

    first commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,15 +4,8 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBP_Sj5AzKYHHkxZfIOI3ollws8x9EN7VI",
-  authDomain: "react-auth-6f8d3.firebaseapp.com",
-  projectId: "react-auth-6f8d3",
-  storageBucket: "react-auth-6f8d3.appspot.com",
-  messagingSenderId: "257321975243",
-  appId: "1:257321975243:web:eb1fee85f36999b4b1ca4e",
-};
+// 貼上你自己的firebase 應用程式設定檔
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
